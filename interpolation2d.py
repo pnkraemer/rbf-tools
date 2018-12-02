dff809ac73b9e94d322bb609841c5eb65a52988e
Nicholas Krämer <nicholas.kraemer@t-online.de>
2018-12-02 11:58:15 +0100

    test

--- interpolation2d.py
+++ interpolation2d.py
@@ -14,19 +14,41 @@
 ############################################
 
 
+from __future__ import division	#division of integers into decimal
 
-
-import pointsets.pointsets as pts
-import rbfs.rbfs as rbfs
 import numpy as np
+#import scipy.special
+#import scipy.spatial
+
+
+
+#from pointsets import *
+#from rbfs import *
+
+
+def buildKernMtrxMatern2(firstPtset, secondPtset, smoothnessPar, lengthscalePar = 1.0, scalingPar = 1.0, normOrd = None):
+	# def maternkernel(pt1, pt2):
+	# 	distVar = np.linalg.norm(pt1 - pt2, ord = normOrd)
+	# 	if distVar <= 0:
+	# 		return scalingPar**2
+	# 	else:
+ # 			scaledDistVar = np.sqrt(2*smoothnessPar)*distVar / lengthscalePar
+	# 	return scalingPar**2 * 2**(1-smoothnessPar) / scipy.special.gamma(smoothnessPar) \
+	# 		* scaledDistVar**(smoothnessPar) * scipy.special.kv(smoothnessPar, scaledDistVar)
+	lenFirstPts = len(firstPtset)
+	lenSecPts = len(secondPtset)
+	kernelMtrxFrame = np.zeros((lenFirstPts,lenSecPts))
+	for rowIdx in xrange(lenFirstPts):
+		for colIdx in xrange(lenSecPts):
+			kernelMtrxFrame[rowIdx,colIdx] = np.linalg.norm(firstPtset[rowIdx,:] - secondPtset[colIdx,:])
+			# kernelMtrxFrame[rowIdx,colIdx] = maternkernel(firstPtset[rowIdx,:], secondPtset[colIdx,:])
+			# print kernelMtrxFrame[rowIdx,colIdx]
+	return kernelMtrxFrame
 
 
 
 def rhsFct(var1):
 	return var1**2.0 + 2*var1**3.1
-
-
-
 
 
 
@@ -47,27 +69,30 @@ regularityPar = input("\tEnter: ")
 ptsTotal = ptsPerDim**dim
 
 
-ptSet = pts.getptsTensorgrid(ptsTotal, dim)
+ptSet = np.random.rand(ptsTotal, dim)
+print ptSet
 
 
-kernMtrx = rbfs.buildKernMtrxMatern(ptSet, ptSet, regularityPar)
 
 
-vecRhs = rbfs.buildRhs(ptSet, rhsFct)
+kernMtrx = buildKernMtrxMatern2(ptSet, ptSet, regularityPar)
+print kernMtrx
 
-solCoeff = np.linalg.solve(kernMtrx, vecRhs)
+# vecRhs = buildRhs(ptSet, rhsFct)
 
-evalPtSet = pts.getptsRandomshiftlattice(ptsTotal, dim)
-evalKernMtrx = rbfs.buildKernMtrxMatern(evalPtSet, ptSet, regularityPar)
-rbfApprx = evalKernMtrx.dot(solCoeff)
+# solCoeff = np.linalg.solve(kernMtrx, vecRhs)
 
-trueEval = rbfs.buildRhs(evalPtSet, rhsFct)
+# evalPtSet = getptsRandomshiftlattice(ptsTotal, dim)
+# evalKernMtrx = buildKernMtrxMatern(evalPtSet, ptSet, regularityPar)
+# rbfApprx = evalKernMtrx.dot(solCoeff)
 
-apprxError = np.linalg.norm(trueEval - rbfApprx)
+# trueEval = buildRhs(evalPtSet, rhsFct)
+
+# apprxError = np.linalg.norm(trueEval - rbfApprx)
 
 
-print "\nError between function and RBF approx.: \n\terror =", apprxError
-print ""
+# print "\nError between function and RBF approx.: \n\terror =", apprxError
+# print ""
 
 
 
