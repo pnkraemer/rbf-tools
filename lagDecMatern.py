b4d63d5cddf44c9cbdadd783feffcf087d34f1a0
Nicholas Krämer <nicholas.kraemer@t-online.de>
2018-12-15 09:33:16 +0100

    lagrange fct decay

--- lagDecMatern.py
+++ lagDecMatern.py
@@ -15,53 +15,63 @@ from kernelMtrcs import buildKernelMtrx
 from functools import partial
 import matplotlib.pyplot as plt
 
-maternReg = 1.0
+
+
+np.random.seed(15051994)
+np.set_printoptions(precision = 2)
+plt.rcParams.update({'font.size': 16})
+
+
+
+maternReg = 2.0
 maternKernelFixReg = partial(maternKernel, maternReg = maternReg)
 
 dim = 2
-numPts = 150
+numPts = 250
 ptSet = getPtsHalton(numPts, dim)
-
 
 kernelMtrx = buildKernelMtrx(ptSet, ptSet, maternKernelFixReg)
 invKernelMtrx = np.linalg.inv(kernelMtrx)
+rhs = np.zeros((numPts,1))
+rhs[17,0] = 1
 
 # Check decay of Lagrange coefficients
-distVecFrom17 = np.zeros(numPts)
-lagCoeff = invKernelMtrx[:,17]
+distFrom17PtSet = np.zeros(numPts)
+lagCoeff = invKernelMtrx.dot(rhs)
 for idx in range(numPts):
-	distVecFrom17[idx] = np.linalg.norm(ptSet[idx,:] - ptSet[17,:])
+	distFrom17PtSet[idx] = np.linalg.norm(ptSet[idx,:] - ptSet[17,:])
 	lagCoeff[idx] = np.linalg.norm(lagCoeff[idx])
-distSort = np.argsort(distVecFrom17)
+distSortPtSet = np.argsort(distFrom17PtSet)
+
+# Check decay of Lagrange function
+numEvalPts = 250
+evalPtSet = np.random.rand(numEvalPts, dim)
+evalMtrxLeft = buildKernelMtrx(evalPtSet, ptSet, maternKernelFixReg)
+lagFctValues = evalMtrxLeft.dot(invKernelMtrx.dot(rhs))
+distFrom17EvalPtSet = np.zeros(numEvalPts)
+for idx in range(numEvalPts):
+	distFrom17EvalPtSet[idx] = np.linalg.norm(evalPtSet[idx,:] - ptSet[17,:])
+	lagFctValues[idx] = np.linalg.norm(lagFctValues[idx])
+distSortEvalPtSet = np.argsort(distFrom17EvalPtSet)
 
 plt.figure()
-plt.semilogy(distVecFrom17[distSort], lagCoeff[distSort], 'o', markersize = 8, color = "darkslategray")
+plt.semilogy(distFrom17PtSet[distSortPtSet], lagCoeff[distSortPtSet], 'o', markersize = 8, color = "darkslategray", alpha = 0.8)
 plt.grid()
-plt.xlabel("Distance from 17th point")
+plt.xlabel("Distance to 17th point")
 plt.ylabel("Absolute value of coefficient")
 plt.title("Lagrange coefficient decay")
+plt.savefig("figures/decLagCoeff_250_2.png")
 plt.show()
 
 
 
-numEvalPts = 500
-evalPtSet = np.random.rand(numEvalPts, dim)
-evalMtrxLeft = buildKernelMtrx(evalPtSet, ptSet, maternKernelFixReg)
-lagFctValues = evalMtrxLeft.dot(invKernelMtrx[:,17])
-print lagFctValues
-distEvalPtsFrom17 = np.zeros(numEvalPts)
-for idx in range(numEvalPts):
-	distEvalPtsFrom17[idx] = np.linalg.norm(evalPtSet[idx,:] - ptSet[17,:])
-	lagFctValues[idx] = np.linalg.norm(lagFctValues[idx])
-distSortEval = np.argsort(distEvalPtsFrom17)
-
-
 plt.figure()
-plt.semilogy(distEvalPtsFrom17[distSortEval], lagFctValues[distSortEval], 'o', markersize = 8, color = "darkslategray")
+plt.semilogy(distFrom17EvalPtSet[distSortEvalPtSet], lagFctValues[distSortEvalPtSet], 'o', markersize = 8, color = "darkslategray", alpha = 0.8)
 plt.grid()
-plt.xlabel("Distance from 17th point")
-plt.ylabel("Absolute value of coefficient")
-plt.title("Lagrange coefficient decay")
+plt.xlabel("Distance to 17th point")
+plt.ylabel("Absolute value of function value")
+plt.title("Lagrange function decay")
+plt.savefig("figures/decLagFct_250_2.png")
 plt.show()
 
 
